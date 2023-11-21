c11f91f53218b31e270771c5b7bfe7dd756a91aa
BiancaBarrosGithub <146129798+BiancaBarrosGithub@users.noreply.github.com>
2023-11-21 16:17:52 -0300

    Compras serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SessaoComprasComponent } from './sessao-compras/sessao-compras.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SessaoComprasComponent } from './sessao-compras/sessao-compras.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sessao-compras/sessao-compras.component.ts
+++ src/app/sessao-compras/sessao-compras.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2';
+import {OnInit} from '@angular/core';
+import { SessaoService } from './service/sessao.service';
+import { Isessao } from './service/isessao';
 
 @Component({
   selector: 'app-sessao-compras',
   templateUrl: './sessao-compras.component.html',
   styleUrls: ['./sessao-compras.component.scss']
 })
-export class SessaoComprasComponent {
+export class SessaoComprasComponent implements OnInit {
+
+  ngOnInit(): void { this.listar()}
+
+  produtos: Isessao[] = [];
+
+  constructor (private service:SessaoService) { }
+
+  listar() {
+    this.service.listar().subscribe(dados => this.produtos = dados);
+  }
+
+  
+
   Comprar(){
     Swal.fire({
       title: "Você deseja comprar esse Produto?",
